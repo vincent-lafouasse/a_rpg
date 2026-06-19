0c95d38ee82b7c6b396e9307f22ee26bc7f45c21
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-19 03:54:34 +0200

    unconditionally fetch and use libraries, never rely on system install

--- script/fetch_raylib.cmake
+++ script/fetch_raylib.cmake
@@ -1,18 +1,11 @@
 set(RAYLIB_VERSION 5.5)
-find_package(raylib ${RAYLIB_VERSION} QUIET)
-if (NOT raylib_FOUND)
-    include(FetchContent)
-    FetchContent_Declare(
-        raylib
-        DOWNLOAD_EXTRACT_TIMESTAMP OFF
-        URL https://github.com/raysan5/raylib/archive/refs/tags/${RAYLIB_VERSION}.tar.gz
-    )
-    FetchContent_GetProperties(raylib)
-    if (NOT raylib_POPULATED)
-        set(FETCHCONTENT_QUIET NO)
-        FetchContent_MakeAvailable(raylib)
-    endif()
-endif()
+include(FetchContent)
+FetchContent_Declare(
+    raylib
+    DOWNLOAD_EXTRACT_TIMESTAMP OFF
+    URL https://github.com/raysan5/raylib/archive/refs/tags/${RAYLIB_VERSION}.tar.gz
+)
+FetchContent_MakeAvailable(raylib)
 
 if(NOT TARGET raylib::raylib)
     add_library(raylib::raylib ALIAS raylib)
--- script/fetch_tinyxml2.cmake
+++ script/fetch_tinyxml2.cmake
@@ -1,14 +1,12 @@
 set(TINYXML2_VERSION 10.0.0)
-find_package(tinyxml2 ${TINYXML2_VERSION} QUIET)
-if (NOT tinyxml2_FOUND)
-    include(FetchContent)
-    FetchContent_Declare(
-        tinyxml2
-        DOWNLOAD_EXTRACT_TIMESTAMP OFF
-        URL https://github.com/leethomason/tinyxml2/archive/refs/tags/${TINYXML2_VERSION}.tar.gz
-    )
-    set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(tinyxml2)
-    set(BUILD_TESTING ON CACHE BOOL "" FORCE)
-    target_compile_options(tinyxml2 PRIVATE -w)
-endif()
+include(FetchContent)
+FetchContent_Declare(
+    tinyxml2
+    DOWNLOAD_EXTRACT_TIMESTAMP OFF
+    URL https://github.com/leethomason/tinyxml2/archive/refs/tags/${TINYXML2_VERSION}.tar.gz
+)
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+FetchContent_MakeAvailable(tinyxml2)
+set(BUILD_TESTING ON CACHE BOOL "" FORCE)
+
+target_compile_options(tinyxml2 PRIVATE -w)
