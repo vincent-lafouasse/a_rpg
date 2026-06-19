08edadd9c3d6c060ef95a35e24e806cec92d7500
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-19 02:13:32 +0200

    refactor static data

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <raylib.h>
 
-int main() {
-    constexpr int COLS = 10;
-    constexpr int ROWS = 5;
-    constexpr int TILE_SIZE = 64;
-    constexpr int WIN_W = COLS * TILE_SIZE;
-    constexpr int WIN_H = ROWS * TILE_SIZE;
+class Map {
+   public:
+    static constexpr int width = 10;
+    static constexpr int height = 5;
+};
 
-    InitWindow(WIN_W, WIN_H, "hello raylib");
-    SetTargetFPS(60);
+class Renderer {
+   public:
+    static constexpr const char* window_name = "rpg";
+    static constexpr int target_fps = 60;
+    static constexpr int tile_size = 64;
+    static constexpr int window_width = Renderer::tile_size * Map::width;
+    static constexpr int window_height = Renderer::tile_size * Map::height;
+};
+
+int main() {
+    InitWindow(Renderer::window_width, Renderer::window_height,
+               Renderer::window_name);
+    SetTargetFPS(Renderer::target_fps);
 
     Texture2D tile = LoadTexture(
         "assets/kenney_micro-roguelike/Tiles/Colored/tile_0000.png");
@@ -17,14 +27,14 @@ int main() {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        for (int row = 0; row < ROWS; ++row) {
-            for (int col = 0; col < COLS; ++col) {
+        for (int row = 0; row < Map::height; ++row) {
+            for (int col = 0; col < Map::width; ++col) {
                 Rectangle src{0, 0, (float)tile.width, (float)tile.height};
                 Rectangle dst{
-                    (float)(col * TILE_SIZE),
-                    (float)(row * TILE_SIZE),
-                    (float)TILE_SIZE,
-                    (float)TILE_SIZE,
+                    (float)(col * Renderer::tile_size),
+                    (float)(row * Renderer::tile_size),
+                    (float)Renderer::tile_size,
+                    (float)Renderer::tile_size,
                 };
                 DrawTexturePro(tile, src, dst, {0, 0}, 0.0f, WHITE);
             }
