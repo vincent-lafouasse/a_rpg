e8155c9afd69d4d60454dc6df7b0345270ec070a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-19 03:27:05 +0200

    ignore tinyxml2 warnings

--- script/fetch_tinyxml2.cmake
+++ script/fetch_tinyxml2.cmake
@@ -7,5 +7,8 @@ if (NOT tinyxml2_FOUND)
         DOWNLOAD_EXTRACT_TIMESTAMP OFF
         URL https://github.com/leethomason/tinyxml2/archive/refs/tags/${TINYXML2_VERSION}.tar.gz
     )
+    set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
     FetchContent_MakeAvailable(tinyxml2)
+    set(BUILD_TESTING ON CACHE BOOL "" FORCE)
+    target_compile_options(tinyxml2 PRIVATE -w)
 endif()
