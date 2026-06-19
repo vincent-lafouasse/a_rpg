59e719d59a65f40bfb27dbaba2d4d384e892c381
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-19 02:55:22 +0200

    add tiny map

---
+++ assets/maps/colored_tilemap_packed.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="colored_tilemap_packed" tilewidth="8" tileheight="8" tilecount="160" columns="16">
+ <image source="../sprites/kenney_micro-roguelike/Tilemap/colored_tilemap_packed.png" width="128" height="80"/>
+</tileset>
