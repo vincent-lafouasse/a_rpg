1f9f62688a155f9053c5f6117a9ed6c162b2574e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-20 03:28:37 +0200

    mark raylib headers as system to silence irrelevant C warnings

--- script/fetch_raylib.cmake
+++ script/fetch_raylib.cmake
@@ -10,3 +10,7 @@ FetchContent_MakeAvailable(raylib)
 if(NOT TARGET raylib::raylib)
     add_library(raylib::raylib ALIAS raylib)
 endif()
+
+get_target_property(_raylib_includes raylib INTERFACE_INCLUDE_DIRECTORIES)
+set_target_properties(raylib PROPERTIES
+    INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${_raylib_includes}")
