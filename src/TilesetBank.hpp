b088cf20cb082aefcfea62f328c1b8058715d4bd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-21 07:29:37 +0200

    store array of (dummy) tilesets

--- src/TilesetBank.hpp
+++ src/TilesetBank.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <array>
+
 #include "tileset_ids.gen.hpp"
 
-struct Tileset;
+struct Tileset {};
 
 struct TilesetBank {
     static constexpr int N = k_tileset_number;
+
+    std::array<Tileset, N> tilesets;
 };
