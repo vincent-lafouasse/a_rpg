5584021110f5ca138c4b3c69bcd58ceef29e239e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-21 07:48:53 +0200

    make the tileset bank non copyable

--- src/TilesetBank.hpp
+++ src/TilesetBank.hpp
@@ -7,6 +7,12 @@
 struct Tileset {};
 
 struct TilesetBank {
+    TilesetBank() {}
+    TilesetBank(const TilesetBank&) = delete;
+    TilesetBank(const TilesetBank&&) = delete;
+    TilesetBank& operator=(const TilesetBank&) = delete;
+    ~TilesetBank() {}
+
     static constexpr int N = k_tileset_number;
 
     std::vector<Tileset> tilesets;
