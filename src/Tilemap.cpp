c5428c93bd0556a58761cf1ebbd6dfd967a6f99b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-22 02:15:44 +0200

    make the u16 payload const

--- src/Tilemap.cpp
+++ src/Tilemap.cpp
@@ -7,4 +7,4 @@
 // some random data to make sure i can hold a span to static data
 static constexpr std::size_t N = 3;
 
-static constexpr std::array<uint16_t, N> data = {4, 2, 0};
+static constexpr std::array<const Tilemap::TileOffset, N> data = {4, 2, 0};
--- src/Tilemap.hpp
+++ src/Tilemap.hpp
@@ -7,7 +7,7 @@
 struct Tilemap {
     using TileOffset = uint16_t;
 
-    std::span<TileOffset> tiles;
+    std::span<const TileOffset> tiles;
     TilesetId tileset_id;
     int height;
     int width;
