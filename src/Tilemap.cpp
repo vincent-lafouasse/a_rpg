dfeed043087e88a1ec27f9a8e4c94cd6b1531983
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-22 02:18:18 +0200

    implement convenience array&->span transmuter

--- src/Tilemap.cpp
+++ src/Tilemap.cpp
@@ -8,3 +8,12 @@
 static constexpr std::size_t N = 3;
 
 static constexpr std::array<const Tilemap::TileOffset, N> data = {4, 2, 0};
+
+namespace {
+template <std::size_t N>
+std::span<const Tilemap::TileOffset> as_span(
+    const std::array<const Tilemap::TileOffset, N> data)
+{
+    return std::span(data);
+}
+}  // namespace
