8c24d504f11524e8bd72251bd99ba791c35c06a1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-22 02:12:16 +0200

    sketch Tilemap class

---
+++ src/Tilemap.cpp
@@ -0,0 +1,10 @@
+#include "Tilemap.hpp"
+
+#include <array>
+
+// #include "tilemap_data.gen.cin
+
+// some random data to make sure i can hold a span to static data
+static constexpr std::size_t N = 3;
+
+static constexpr std::array<uint16_t, N> data = {4, 2, 0};
---
+++ src/Tilemap.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <span>
+
+#include "tileset_ids.gen.hpp"
+
+struct Tilemap {
+    using TileOffset = uint16_t;
+
+    std::span<TileOffset> tiles;
+    TilesetId tileset_id;
+    int height;
+    int width;
+    int tile_size;
+};
