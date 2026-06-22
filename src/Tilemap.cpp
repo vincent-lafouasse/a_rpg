5e695904f7e8c7e01c1bb5267de36b74e62aced9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-23 00:07:16 +0200

    remove stub

--- src/Tilemap.cpp
+++ src/Tilemap.cpp
@@ -8,14 +8,3 @@
 static constexpr std::size_t N = 3;
 
 static constexpr std::array<const Tilemap::TileOffset, N> data = {4, 2, 0};
-
-namespace {
-template <std::size_t N>
-std::span<const Tilemap::TileOffset> as_span(
-    const std::array<const Tilemap::TileOffset, N> data)
-{
-    // i'm going to trash this function anyways
-    foo();
-    return std::span(data);
-}
-}  // namespace
