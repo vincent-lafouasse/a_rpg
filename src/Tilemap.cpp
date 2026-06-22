87dcd77d71d56d2beb6d8e199458022d2b0a89ee
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2026-06-23 00:31:33 +0200

    construct actual tilemap from actual data, remove dummy tilemap

--- src/Tilemap.cpp
+++ src/Tilemap.cpp
@@ -1,22 +1,12 @@
 #include "Tilemap.hpp"
 
-#include <array>
-
 #include "tilemap_data.gen.inc"
 
-// some random data to make sure i can hold a span to static data
-static constexpr std::size_t N = 3;
-
-static constexpr int k_height = 1;
-static constexpr int k_width = 3;
-
-static constexpr std::array<const Tilemap::TileOffset, N> data = {4, 2, 0};
-
 Tilemap::Tilemap()
-    : tiles(data),
-      tileset_id(TilesetId::colored_tilemap_packed),
-      height(k_height),
-      width(k_width),
-      tile_size(420)
+    : tiles(k_tile_data),
+      tileset_id(k_tilemap_tileset),
+      height(k_tilemap_height),
+      width(k_tilemap_width),
+      tile_size(k_tilemap_tile_size)
 {
 }
